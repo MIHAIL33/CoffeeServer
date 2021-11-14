c3c988f6da005b35e184ac060b0b4aafdcad327c
MIHAIL33 <mihail_was96@mail.ru>
2021-11-14 21:56:58 +0300

    Inject

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,14 +1,39 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module, Scope } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
+import { Connection } from 'typeorm';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+// class MockCoffeesService {}
+// providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }]
+//{ provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe']}
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
+@Injectable()
+export class CoffeeBrandsFactory {
+    async create(): Promise<string[]> {
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe'])
+        return coffeeBrands
+    }
+}
+
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
-    providers: [CoffeesService]
+    providers: [
+        CoffeesService,
+        CoffeeBrandsFactory,
+        { 
+            provide: ConfigService, 
+            useClass: process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService},
+        { provide: COFFEE_BRANDS, useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(), inject: [CoffeeBrandsFactory], scope: Scope.TRANSIENT}
+    ],
+    exports: [CoffeesService]
 })
 export class CoffeesModule {}
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,14 +1,15 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable, NotFoundException, Scope } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Event } from 'src/events/entities/event.entity';
 import { Connection, Repository } from 'typeorm';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
-@Injectable()
+@Injectable( {scope: Scope.REQUEST} ) //{scope: Scope.TRANSIENT}
 export class CoffeesService {
 
     constructor(
@@ -16,8 +17,11 @@ export class CoffeesService {
         private readonly coffeeRepository: Repository<Coffee>,
         @InjectRepository(Flavor)
         private readonly flavorRepository: Repository<Flavor>,
-        private readonly connection: Connection
-    ) {}
+        private readonly connection: Connection,
+        @Inject(COFFEE_BRANDS) coffeeBrands: string[]
+    ) {
+        console.log(coffeeBrands)
+    }
 
     findAll(paginationQuery: PaginationQueryDto) {
         const { limit, offset } = paginationQuery
